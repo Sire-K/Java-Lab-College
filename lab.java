ae02114ae077aa34e3250a934d0594dfbbef67b7
Sire-K <150567674+Sire-K@users.noreply.github.com>
2025-04-27 17:38:15 +0545

    Update lab.java

--- lab.java
+++ lab.java
@@ -1,16 +1,30 @@
-// Lab Report 1
 import java.util.Scanner;
 
 class lab {
     public static void main(String[] args) {
         System.err.println("Enter two numbers: ");
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        System.out.println("The addition is: " + (a + b));
-        System.out.println("The subtraction is: " + (a - b));
-        System.out.println("The multiplication is: " + (a * b));
-        System.out.println("The division is: " + (a / b));
+
+        if (scanner.hasNextInt()) {
+            int a = scanner.nextInt();
+            if (scanner.hasNextInt()) {
+                int b = scanner.nextInt();
+
+                System.out.println("The addition is: " + (a + b));
+                System.out.println("The subtraction is: " + (a - b));
+                System.out.println("The multiplication is: " + (a * b));
+
+                if (b != 0) {
+                    System.out.println("The division is: " + (a / b));
+                } else {
+                    System.out.println("Division by zero is not allowed.");
+                }
+            } else {
+                System.err.println("Invalid input. Please enter integers only.");
+            }
+        } else {
+            System.err.println("Invalid input. Please enter integers only.");
+        }
         scanner.close();
     }
 }
